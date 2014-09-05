c8c882b1537fe8bd922095ce93750e3e4736e921
ctg168 <ctg168@gmail.com>
2014-09-06 02:09:59 +0800

    light & dark theme ok.

--- app/src/main/java/com/jet2006/styletest002/MyActivity.java
+++ app/src/main/java/com/jet2006/styletest002/MyActivity.java
@@ -2,35 +2,32 @@ package com.jet2006.styletest002;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
 
+    TextView tvSwitchTheme;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ThemeUtil.onActivityCreateSetTheme(this);
         setContentView(R.layout.fontstyle_example);
+
+        tvSwitchTheme = (TextView) findViewById(R.id.tvSwitchTheme);
+
+        tvSwitchTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SwitchTheme();
+            }
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+    private void SwitchTheme() {
+        ThemeUtil.changeTheme(this);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
